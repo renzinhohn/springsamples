ffa1f505dde20386a433bad518531b4fe064df20
renzinhohn <renzo.hn@gmail.com>
2022-11-05 17:20:32 -0500

    java8training: Uploading Java 8 exercises

---
+++ java8training/src/main/java/com/renzo/quickstart/java8training/App.java
@@ -0,0 +1,13 @@
+package com.renzo.quickstart.java8training;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
