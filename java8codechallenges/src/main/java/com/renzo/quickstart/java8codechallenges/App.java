3d9553f1e3ad3a49ba44aefe21455de43746b983
renzinhohn <renzo.hn@gmail.com>
2022-12-29 00:30:00 -0500

    java8codechallenges: Creating new branch and new project called java8codechallenges from Latitude7 laptop

---
+++ java8codechallenges/src/main/java/com/renzo/quickstart/java8codechallenges/App.java
@@ -0,0 +1,13 @@
+package com.renzo.quickstart.java8codechallenges;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
